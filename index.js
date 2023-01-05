72ea78e976f711ce70da4cb3ed83ab155236af9d
Devendra Johari <54894480+DevendraJohari24@users.noreply.github.com>
2023-01-05 22:37:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin:"http://localhost:3000",
+  methods: ["GET","POST", "PUT", "DELETE", "PATCH"]
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
